af578a05072cefd06146fefec6b7dd8dea867893
renatocortez <recortez@gmail.com>
2021-08-14 17:07:07 -0300

    exercicios

---
+++ ex010.py
@@ -0,0 +1,4 @@
+print('$$$ CONVERSOR DE REAL PARA DÓLAR $$$')
+n = float(input('Digite o valor que deseja fazer a conversão: '))
+d = n / 5.19
+print('O valor de R${} reais equivale a US${:.2f} dólares'.format(n, d))
