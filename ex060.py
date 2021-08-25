a7f3f80618b547f3d400c1b659bc5f3db0ae2d4d
renatocortez <recortez@gmail.com>
2021-08-25 15:12:33 -0300

    exercicios

--- ex060.py
+++ ex060.py
@@ -1,2 +1,12 @@
 '''FAÇA UM PROGRAMA QUE LEIA UM NÚMERO QUALQUER E MOSTRE O SEU FATORIAL'''
 
+n = int(input('Digite um número para saber o seu fatorial: '))
+c = n
+f = 1
+print(f'Calculando {n}!', end='')
+while c > 0:
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print(f'{f}')
