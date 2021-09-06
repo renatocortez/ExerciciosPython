eab79b2801ab055736e4a227c4a3e963b3e35861
renatocortez <recortez@gmail.com>
2021-09-06 17:12:30 -0300

    Create ex078.py

---
+++ ex078.py
@@ -0,0 +1,30 @@
+'''FAÇA UM PROGRAMA QUE LEIA 5 VALORES NÚMERICOS
+E GUARDE-OS EM UMA LISTA. NO FINAL, MOSTRE QUAL FOI O
+MAIOR E O MENOR VALOR DIGITADO E AS SUAS RESPECTIVAS
+POSIÇÕES NA LISTA'''
+
+maior = 0
+menor = 0
+
+listanum = []
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}:'))) #ADICIONA VALORES NA LISTA
+    if c == 0: #QUANDO C FOR O PRIMEIRO VALOR LIDO
+        maior = menor = listanum[c] # ELE É O MAIOR E O MENOR
+    else:
+        if listanum[c] > maior:
+            maior = listanum[c]
+        if listanum[c] < menor:
+            menor = listanum[c]
+print('=-' * 30)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
+for i, v in enumerate(listanum): #VARRE A LISTA PARA ACHAR AS POSIÇÕES DOS VALORES
+    if v == maior:
+        print(f'{i}...', end='')
+print()
+print(f'O menor valor digitado foi {menor} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == menor:
+        print(f'{i}...', end='')
+print()
