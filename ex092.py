69e92d55008e7dc1ae362f06aaca55b37ec64e1b
renatocortez <recortez@gmail.com>
2021-09-13 17:58:21 -0300

    Create ex092.py

---
+++ ex092.py
@@ -0,0 +1,18 @@
+''' Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente,
+além da idade, com quantos anos a pessoa vai se aposentar.'''
+
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de trabalho[Não possui -> 0]: '))
+if dados['ctps'] != 0:
+    dados['contratacao'] = int(input('Ano de contratação: '))
+    dados['salario'] = float(input('Salário: '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratacao'] + 35) - datetime.now().year)
+print('-=' * 30)
+for k, v in dados.items():
+    print(f'    - {k} tem o valor {v}')
+
+    
