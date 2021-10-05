57a156a9d9257f32d7341a766aabe3ac6a5d35f9
renatocortez <recortez@gmail.com>
2021-10-05 20:03:20 -0300

    Update ex093.py

--- ex093.py
+++ ex093.py
@@ -9,7 +9,7 @@ partidas = list()
 jogador['nome'] = str(input('Nome do Jogador: '))
 tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(0, tot):
-    partidas.append(int(input(f'    Quantos gols na partida {c}?')))
+    partidas.append(int(input(f'    Quantos gols na partida {c + 1}?')))
 jogador['gols'] = partidas[:]
 jogador['total'] = sum(partidas)
 print('-=' * 30)
@@ -20,6 +20,6 @@ for k, v in jogador.items():
 print('-=' * 30)
 print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
 for i, v in enumerate(jogador['gols']):
-    print(f'    => partida {i}, fez {v} gols.')
+    print(f'    => partida {i + 1}, fez {v} gols.')
 print(f'Foi um total de {jogador["total"]} gols.')
 
