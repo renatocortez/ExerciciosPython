6d4695954d2650744cc513f9fa0f3300aa51db7f
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-17 20:46:47 -0300

    Conversor de metros - cm - mm
    '''FAÇA UM PROGRAMA QUE CONVERTA METROS PARA CENTÍMETROS E MILÍMETROS'''

--- ex008.py
+++ ex008.py
@@ -1,4 +1,4 @@
-print('##### CONVERSOR DE METROS PARA CM E MM #####')
+print('###### CONVERSOR DE METROS PARA CM E MM ######')
 n = float(input('Digite o valor desejado em metros: '))
 cm = n * 100
 mm = n * 1000
