0e87144f5840345a6f7a3e67596728c4a57c2987
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-18 19:13:40 -0300

    Conversor de temperatura
    '''CONVERTE A TEMPERATURA DE CELCIUS PARA FAHRENHEIT'''

--- ex014.py
+++ ex014.py
@@ -1,4 +1,4 @@
-print('!!!!! CONVERSOR DE TEMPERATURA -> Celcius -> Fahrenehit !!!!!')
+print('!!!!!! CONVERSOR DE TEMPERATURA -> Celcius -> Fahrenehit !!!!!!')
 n = float(input('Digite a temperatura em Celcius: '))
 conv = (n * 9 / 5 ) + 32
 print('A temperatura de {} em Celcius equivale a {} Fahrenheit.'.format(n, conv))
