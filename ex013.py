b42a7e3adeba0caae42f6a8b8adafe25646200e5
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-18 19:12:19 -0300

    Aumento de salário
    '''CALCULA UM SALÁRIO QUALQUER COM 15% DE AUMENTO'''

--- ex013.py
+++ ex013.py
@@ -1,4 +1,4 @@
-print('+++++ AUMENTO DE 15% NO SALÁRIO +++++')
+print('++++++ AUMENTO DE 15% NO SALÁRIO ++++++')
 n = float(input('Digite seu salário:R$ '))
 sal = n + (n * 15 /100)
 print('O seu salário com 15% de aumento é de R${:.2f}'.format(sal))
