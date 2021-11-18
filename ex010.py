73ad3ca06b55ce7a4789ffb15e357cb736df0558
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-18 19:05:58 -0300

    Conversor de moeda
    '''CONVERTE A MOEDA REAL(BR) EM DÓLARES(USA)'''

--- ex010.py
+++ ex010.py
@@ -1,4 +1,4 @@
 print('$$$ CONVERSOR DE REAL PARA DÓLAR $$$')
 n = float(input('Digite o valor que deseja fazer a conversão: '))
-d = n / 5.19
+d = n / 5.65
 print('O valor de R${} reais equivale a US${:.2f} dólares'.format(n, d))
