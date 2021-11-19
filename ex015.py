98a67d3c61f1f4a48095dd2e0d3313891d5187ec
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-19 10:59:35 -0300

    Valor de aluguel de carro
    '''ESCREVA UM PROGRAMA QUE PERGUNTE A
    QUANTIDADE DE KM PERCORRIDOS POR UM CARRO
    ALUGADO E A QUANTIDADE DE DIAS PELOS QUAIS ELE
    FOI ALUGADO.CALCULE O PREÇO A PAGAR, SABENDO
    QUE O CARRO CUSTA R$60 POR DIA E r$0,15 POR KM
    RODADO.'''

--- ex015.py
+++ ex015.py
@@ -1,10 +1,3 @@
-'''ESCREVA UM PROGRAMA QUE PERGUNTE A
-QUANTIDADE DE KM PERCORRIDOS POR UM CARRO
-ALUGADO E A QUANTIDADE DE DIAS PELOS QUAIS ELE
-FOI ALUGADO.CALCULE O PREÇO A PAGAR, SABENDO
-QUE O CARRO CUSTA R$60 POR DIA E r$0,15 POR KM
-RODADO.'''
-
 km = float(input('Quantos quilômetros foram percorridos? '))
 dias = int(input('Quantos dias foram usados? '))
 aluguel = (dias * 60) + (km * 0.15)
