4e93ec21e6a44ea3c57c122fde7049c782c89218
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-19 11:01:43 -0300

    Triângulo, catetos, hipotenusa
    '''LER O COMPRIMENTO DO CATETO OPOSTO E DO CATETO ADJACENTE DE UM
    TRIÂNGULO RETÂNGULO, CALCULE E MOSTRE O COMPRIMENTO DA HIPOTENUSA'''

--- ex017.py
+++ ex017.py
@@ -1,6 +1,3 @@
-'''LER O COMPRIMENTO DO CATETO OPOSTO E DO CATETO ADJACENTE DE UM
-TRIÂNGULO RETÂNGULO, CALCULE E MOSTRE O COMPRIMENTO DA HIPOTENUSA'''
-
 from math import hypot
 catop = float(input('Digite o comprimento do cateto oposto: '))
 catadj = float(input('Digite o comprimento do cateto adjacente: '))
