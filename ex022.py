872a94f1c02c0a73ea8508e3aba9230337322606
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-23 18:34:54 -0300

    Formatando nomes, upper, lower, len, count, find
    '''CRIE UM PROGRAMA QUE LEIA O NOME COMPLETO DE UMA PESSOA E MOSTRE:
    1-O NOME COM TODAS AS LETRAS MAIÚSCULAS
    2-O NOME COM TODAS MINÚSCULAS
    3-QUANTAS LETRAS AO TODO(SEM CONSIDERAR OS ESPAÇOS)
    4 - QUANTAS LETRAS TEM O PRIMEIRO NOME'''

--- ex022.py
+++ ex022.py
@@ -1,9 +1,3 @@
-'''CRIE UM PROGRAMA QUE LEIA O NOME COMPLETO DE UMA PESSOA E MOSTRE:
-1-O NOME COM TODAS AS LETRAS MAIÚSCULAS
-2-O NOME COM TODAS MINÚSCULAS
-3-QUANTAS LETRAS AO TODO(SEM CONSIDERAR OS ESPAÇOS)
-4 - QUANTAS LETRAS TEM O PRIMEIRO NOME'''
-
 nome = str(input('Digite seu nome completo: '))
 print('Seu nome em letras maiúsculas é:{}'.format(nome.upper()))
 print('Seu nome em letras minúsculas é:{}'.format(nome.lower()))
