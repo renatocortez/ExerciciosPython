1e367dd196606cb1d8817aa4667d16c121f43668
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-23 18:38:09 -0300

    Lendo um nome específico
    '''CRIE UM PROGRAMA QUE LEIA O NOME DE UMA CIDADE E DIGA SE ELA
    COMEÇA OU NÃO COM O NOME SANTO'''

--- ex024.py
+++ ex024.py
@@ -1,6 +1,3 @@
-'''CRIE UM PROGRAMA QUE LEIA O NOME DE UMA CIDADE E DIGA SE ELA
-COMEÇA OU NÃO COM O NOME SANTO'''
-
 cidade = str(input('Digite o nome de qualquer cidade: ')).strip()
 print(cidade[:5].upper() == 'SANTO')
 
