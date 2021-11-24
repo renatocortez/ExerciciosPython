d5f17d4e4d8c2b01ee91751f179144cc7198aaa6
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-24 18:45:57 -0300

    Posição de letra em uma string
    '''LEIA UMA FRASE E MOSTRE
    1-QUANTAS VEZES APARECE A LETRA A
    2-EM QUE POSIÇÃO ELA APARECE A PRIMEIRA VEZ
    3 - EM QUE POSIÇÃO ELA APARECE A ÚLTIMA VEZ'''

--- ex026.py
+++ ex026.py
@@ -1,8 +1,3 @@
-'''LEIA UMA FRASE E MOSTRE
-1-QUANTAS VEZES APARECE A LETRA A
-2-EM QUE POSIÇÃO ELA APARECE A PRIMEIRA VEZ
-3 - EM QUE POSIÇÃO ELA APARECE A ÚLTIMA VEZ'''
-
 frase = str(input('Digite uma frase: ')).upper().strip()
 print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
 print('A letra A aparece na {}º posição a primeira vez.'.format(frase.find('A')+1))
