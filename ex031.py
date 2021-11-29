aa3d71dac4a1dcf57793ec8d1c6382b794394b1c
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-29 18:40:57 -0300

    Calcula o preço de passagem
    '''DESENVOLVA UM PROGRAMA QUE PERGUNTE A DISTÂNCIA DE UMA VIAGEM EM KM.
    CALCULE O PREÇO DA PASSAGEM COBRANDO R$0.50 POR KM PARA VIAGENS ATÉ 200KM E R$0.45 PARA VIAGENS MAIS LONGAS'''

--- ex031.py
+++ ex031.py
@@ -1,6 +1,3 @@
-'''DESENVOLVA UM PROGRAMA QUE PERGUNTE A DISTÂNCIA DE UMA VIAGEM EM KM.
-CALCULE O PREÇO DA PASSAGEM COBRANDO R$0.50 POR KM PARA VIAGENS ATÉ 200KM E R$0.45 PARA VIAGENS MAIS LONGAS'''
-
 distancia = float(input('Qual é a distância percorrida? '))
 if distancia <= 200:
     preco = distancia * 0.50
