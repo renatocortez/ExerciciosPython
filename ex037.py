5f62fdd2f9626240cd0523770bc858513e8d591a
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-11-30 18:11:24 -0300

    Base de conversão para um número
    '''ESCREVA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E PEÇA
    PARA O USUÁRIO ESCOLHER QUAL SERÁ A BASE DE CONVERSÃO:
    1-BINÁRIO
    2-OCTAL
    3 – HEXADECIMAL'''

--- ex037.py
+++ ex037.py
@@ -1,9 +1,3 @@
-'''ESCREVA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E PEÇA
-PARA O USUÁRIO ESCOLHER QUAL SERÁ A BASE DE CONVERSÃO:
-1-BINÁRIO
-2-OCTAL
-3 – HEXADECIMAL'''
-
 n = int(input('Digite um número: '))
 print('Opções:\n(1) - Binário\n(2) - Octal\n(3) - Hexadecimal')
 opcao = int(input('Digite a opção desejada: '))
