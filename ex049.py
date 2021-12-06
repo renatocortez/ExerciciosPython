3ef4207115c878d4e70352446915e8b93033dbdc
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2021-12-06 19:02:53 -0300

    Tabuada com For
    '''REFAÇA O DESAFIO 009, MOSTRANDO A TABUADA DE UM NÚMERO QUE O
    USUÁRIO ESCOLHER, SÓ QUE AGORA UTILIZANDO UM LAÇO FOR'''

--- ex049.py
+++ ex049.py
@@ -1,6 +1,3 @@
-'''REFAÇA O DESAFIO 009, MOSTRANDO A TABUADA DE UM NÚMERO QUE O
-USUÁRIO ESCOLHER, SÓ QUE AGORA UTILIZANDO UM LAÇO FOR'''
-
 print('-=' * 5, "Tabuada v2", "-=" * 5)
 
 n = int(input('Digite um valor para saber a tabuada: '))
