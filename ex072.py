35340db5377ac23464313a2efef75c0819253911
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-18 13:25:04 -0300

    Número por extenso com Tuplas
    '''CRIE UM PROGRAMA QUE TENHA UMA TUPLA
    TOTALMENTE PREENCHIDA COM UMA CONTAGEM POR EXTENSO,
    DE ZERO ATÉ VINTE.
    O PROGRAMA DEVERÁ LER UM NÚMERO PELO TECLADO(0-20)
    E MOSTRÁ-LO POR EXTENSO.'''

--- ex072.py
+++ ex072.py
@@ -1,9 +1,3 @@
-'''CRIE UM PROGRAMA QUE TENHA UMA TUPLA
-TOTALMENTE PREENCHIDA COM UMA CONTAGEM POR EXTENSO,
-DE ZERO ATÉ VINTE.
-O PROGRAMA DEVERÁ LER UM NÚMERO PELO TECLADO(0-20)
-E MOSTRÁ-LO POR EXTENSO.'''
-
 cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis',
 'sete', 'oito', 'nove','dez', 'onze','doze', 'treze', 'catorze',
 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
