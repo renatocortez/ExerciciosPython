dfc962f0707390bc7cbf14c4c51b577d294b93ac
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-18 13:27:31 -0300

    Números com tuplas
    '''Desenvolva um programa que leia quatro valores pelo
     teclado e guarde-os em uma tupla. No final, mostre:
    
    A) Quantas vezes apareceu o valor 9.
    
    B) Em que posição foi digitado o primeiro valor 3.
    
    C) Quais foram os números pares.'''

--- ex075.py
+++ ex075.py
@@ -1,12 +1,3 @@
-'''Desenvolva um programa que leia quatro valores pelo
- teclado e guarde-os em uma tupla. No final, mostre:
-
-A) Quantas vezes apareceu o valor 9.
-
-B) Em que posição foi digitado o primeiro valor 3.
-
-C) Quais foram os números pares.'''
-
 num = (int(input('Digite um número: ')),
     int(input('Digite outro número:')),
     int(input('Digite mais um número: ')),
