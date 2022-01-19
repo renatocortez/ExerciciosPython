2ccc4c11975111c4435791115fdf5a1d796df281
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-19 16:53:14 -0300

    Números decrescentes e quantidade
    '''Crie um programa que vai ler vários números e colocar em uma lista.
    Depois disso, mostre:
    A) Quantos números foram digitados.
    B) A lista de valores, ordenada de forma decrescente.
    C) Se o valor 5 foi digitado e está ou não na lista.'''

--- ex081.py
+++ ex081.py
@@ -1,9 +1,3 @@
-'''Crie um programa que vai ler vários números e colocar em uma lista.
-Depois disso, mostre:
-A) Quantos números foram digitados.
-B) A lista de valores, ordenada de forma decrescente.
-C) Se o valor 5 foi digitado e está ou não na lista.'''
-
 valores = []
 while True:
     valores.append(int(input('Digite um valor: ')))
