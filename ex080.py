7749affe17319eeac0b104558415c7b8417d7449
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-19 16:50:57 -0300

    Lista ordenada
    '''Crie um programa onde o usuário possa digitar
    cinco valores numéricos e cadastre-os em uma lista
    já na posição correta de inserção (sem usar o sort()).
    No final, mostre a lista ordenada na tela.'''

--- ex080.py
+++ ex080.py
@@ -1,8 +1,3 @@
-'''Crie um programa onde o usuário possa digitar
-cinco valores numéricos e cadastre-os em uma lista
-já na posição correta de inserção (sem usar o sort()).
-No final, mostre a lista ordenada na tela.'''
-
 lista = []
 for c in range(0, 5):
     n = int(input('Digite um valor: '))
