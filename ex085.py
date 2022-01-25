b6ad53e135f2aa5927717a202e71c911685d6bd7
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-25 17:50:38 -0300

    Valores pares e ímpares - listas
    '''Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
    No final, mostre os valores pares e ímpares em ordem crescente.'''

--- ex085.py
+++ ex085.py
@@ -1,6 +1,3 @@
-'''Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
-No final, mostre os valores pares e ímpares em ordem crescente.'''
-
 num = [[], []]
 valor = 0
 for c in range(1, 8):
