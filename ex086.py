e636cc9a48fa743e6602c8ced6f046cfd613e932
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-25 17:51:26 -0300

    Matriz 3x3
    '''Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado.
    No final, mostre a matriz na tela, com a formatação correta.'''

--- ex086.py
+++ ex086.py
@@ -1,6 +1,3 @@
-'''Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado.
-No final, mostre a matriz na tela, com a formatação correta.'''
-
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
     for c in range(0, 3):
