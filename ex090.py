3a359a662154351acf756cc3f3a7a182ae686e38
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-01-26 17:17:56 -0300

    Nome e média com dicionário
    ''' Faça um programa que leia nome e média de um aluno,
    guardando também a situação em um dicionário. No final,
    mostre o conteúdo da estrutura na tela.'''

--- ex090.py
+++ ex090.py
@@ -1,7 +1,3 @@
-''' Faça um programa que leia nome e média de um aluno,
-guardando também a situação em um dicionário. No final,
-mostre o conteúdo da estrutura na tela.'''
-
 aluno = dict()
 aluno['nome'] = str(input('Nome: '))
 aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
