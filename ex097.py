d089a2ce23eff6e07c6404e41b4a8b20af597484
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-02-02 19:42:02 -0300

    Texto adaptado com funções
    '''FAÇA UM PROGRAMA QUE TENHA UMA FUNÇÃO CHAMADA ESCREVA(),
    QUE RECEBA UM TEXTO QUALQUER COMO PARÂMETRO E MOSTRE UMA MENSAGEM COM
    TAMANHO ADAPTÁVEL.'''

--- ex097.py
+++ ex097.py
@@ -1,7 +1,3 @@
-'''FAÇA UM PROGRAMA QUE TENHA UMA FUNÇÃO CHAMADA ESCREVA(),
-QUE RECEBA UM TEXTO QUALQUER COMO PARÂMETRO E MOSTRE UMA MENSAGEM COM
-TAMANHO ADAPTÁVEL.'''
-
 def escreva(msg):
     tam = len(msg) + 4
     print('~' * tam)
