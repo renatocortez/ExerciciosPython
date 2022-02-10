6ae088044f16423b0ef3b041d94b534daee746d3
Renato Cortez <33463623+renatocortez@users.noreply.github.com>
2022-02-10 20:31:00 -0300

    Fatorial com funções
    '''Crie um programa que tenha uma função  fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e
    outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não  na tela o processo de cálculo do fatorial.'''

--- ex102.py
+++ ex102.py
@@ -1,8 +1,3 @@
-'''Crie um programa que tenha uma função
- fatorial() que receba dois parâmetros:o primeiro que indique o número a calcular e
-outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não
- na tela o processo de cálculo do fatorial.'''
-
 def fatorial(n, show=False):
     f = 1
     for c in range(n, 0, -1):
